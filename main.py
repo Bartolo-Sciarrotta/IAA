8d04f612452b2cb47e72927339d39b88c43a1a8a
Bartolo-Sciarrotta <138524771+Bartolo-Sciarrotta@users.noreply.github.com>
2024-11-22 19:15:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def configurar_pagina():
     #CAMBIAR NOMBRE DE PESTAÑA
     st.set_page_config("Mi chat AI")
     #AGREGAMOS UN TITULO
-    st.title("Claudio")
+    st.title("Amarildo")
     #AGREGAR SIDEBAR
     st.sidebar.title("Sidebar de modelos")
     m = st.sidebar.selectbox("Modelos", MODELOS, index = 0)
